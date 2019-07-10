2d040570659f26a59cb40f3a0bb3c444adbb683c
Danielius <dan.asmontas@gmail.com>
2019-07-10 22:06:55 +0300

    field injection

--- app/src/main/java/io/visiogen/dagger2application/MainActivity.kt
+++ app/src/main/java/io/visiogen/dagger2application/MainActivity.kt
@@ -4,15 +4,19 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import io.visiogen.dagger2application.di.CarComponent
 import io.visiogen.dagger2application.di.DaggerCarComponent
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var car:Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val component:CarComponent = DaggerCarComponent.create()
-        val car:Car = component.getCar()
+        //val car:Car = component.getCar()
+        component.inject(this)
         car.drive()
     }
 }
--- app/src/main/java/io/visiogen/dagger2application/di/CarComponent.kt
+++ app/src/main/java/io/visiogen/dagger2application/di/CarComponent.kt
@@ -2,9 +2,12 @@ package io.visiogen.dagger2application.di
 
 import dagger.Component
 import io.visiogen.dagger2application.Car
+import io.visiogen.dagger2application.MainActivity
 
 @Component
 interface CarComponent {
 
     fun getCar():Car
+
+    fun inject(mainActivity:MainActivity)
 }
